0d49467387885fc505dd0222dd2650dcbb6b85f9
NikhilPrajith <57269214+NikhilPrajith@users.noreply.github.com>
2021-03-18 23:41:20 -0400

    Update1

--- InstagramClone/AppDelegate.swift
+++ InstagramClone/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // --- Copy this only
+            
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "sUMHHUZkSq6vjlsxbNK5PplZgvDuPKRViKxYbT0I" // <- UPDATE
+                $0.clientKey = "mZAR6B9s4TjFidNbqF47yqVNr1tDiJMTfVGVmvdR" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+            
+            // --- end copy
         return true
     }
 
